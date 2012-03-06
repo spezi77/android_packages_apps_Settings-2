eaf46f2368ca76c170b50bb555a80cb615186c6b
Danesh M <daneshm90@gmail.com>
2012-03-05 21:38:27 -0500

    Settings : NavigationBar Customization
    Contains:
    
    - Settings side to customize the navigation bar buttons.
      Id: I39d9362f1930dc4181d20ff95077b4c290f6907e
    
    - Settings : NavigationBar Customization
      Settings side to customize the navigation bar buttons.
      Id: Ibd51fbca96d5f0d5e75c7bd74dc5ec62ed1d969d
    
    - Settings: Fix NavBar lock button when resetting default
      When the NavBar is reset to defaults while in edit mode, lock icon should
      re-lock
      Id: I876725046e380ebb7da774ae412ce2b68f5310f8
    
    - And a few small fixes
    
    - Add support for navbar editing on tablets
    
    - Add npe check
    
    Change-Id: Ic77ca21d976015a1f4b4e10cab27fa35760035da
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	src/com/android/settings/cyanogenmod/SystemSettings.java

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2012 The CyanogenMod project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright (C) 2012 The CyanogenMod project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package com.android.settings.cyanogenmod;
 
@@ -29,6 +29,7 @@ import android.view.IWindowManager;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -39,6 +40,7 @@ public class SystemSettings extends SettingsPreferenceFragment {
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
     private static final String KEY_HARDWARE_KEYS = "hardware_keys";
+    private static final String KEY_NAVIGATION_BAR = "navigation_bar";
     private static final String KEY_LOCK_CLOCK = "lock_clock";
 
     private PreferenceScreen mNotificationPulse;
@@ -60,7 +62,6 @@ public class SystemSettings extends SettingsPreferenceFragment {
                 getPreferenceScreen().removePreference(mNotificationPulse);
             } else {
                 updateLightPulseDescription();
-
             }
         }
 
@@ -77,31 +78,27 @@ public class SystemSettings extends SettingsPreferenceFragment {
 
         // Only show the hardware keys config on a device that does not have a navbar
         // Only show the navigation bar config on phones that has a navigation bar
-  //      boolean removeKeys = false;
-  //      boolean removeNavbar = false;
-  //      IWindowManager windowManager = IWindowManager.Stub.asInterface(
-  //              ServiceManager.getService(Context.WINDOW_SERVICE));
-  //      try {
-  //          if (windowManager.hasNavigationBar()) {
-  //              removeKeys = true;
-  //              if (Utils.isTablet(getActivity())) {
-  //                  removeNavbar = true;
-  //             }
-  //          } else {
-  //              removeNavbar = true;
-  //          }
-  //      } catch (RemoteException e) {
+        boolean removeKeys = false;
+        boolean removeNavbar = false;
+        IWindowManager windowManager = IWindowManager.Stub.asInterface(
+                ServiceManager.getService(Context.WINDOW_SERVICE));
+        try {
+            if (windowManager.hasNavigationBar()) {
+                removeKeys = true;
+            } else {
+                removeNavbar = true;
+            }
+        } catch (RemoteException e) {
             // Do nothing
-   //     }
+        }
 
         // Act on the above
-      //  if (removeKeys) {
-       //     getPreferenceScreen().removePreference(findPreference(KEY_HARDWARE_KEYS));
-      //  }
-     //   if (removeNavbar) {
-       //     getPreferenceScreen().removePreference(findPreference(KEY_NAVIGATION_BAR));
-     //   }
-
+        if (removeKeys) {
+            getPreferenceScreen().removePreference(findPreference(KEY_HARDWARE_KEYS));
+        }
+        if (removeNavbar) {
+            getPreferenceScreen().removePreference(findPreference(KEY_NAVIGATION_BAR));
+        }
     }
 
     private void updateLightPulseDescription() {
