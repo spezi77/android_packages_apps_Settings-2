Hellybean/android_packages_apps_Settings

lockscreen text and image color settings
Change-Id: Icfb127850768bfab8e8b59ea7f61037241d4a9bf

src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -53,6 +53,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     public static final String KEY_BACKGROUND_PREF = "lockscreen_background";
     public static final String KEY_SEE_TRHOUGH_PREF = "lockscreen_see_through";
     public static final String KEY_STYLE_PREF = "lockscreen_style";
+    public static final String KEY_TEXT_COLOR = "lockscreen_custom_text_color";
     private static final int LOCK_STYLE_JB = 0;  
     private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
     private static final String KEY_CLOCK_ALIGN = "lockscreen_clock_align";
@@ -62,6 +63,7 @@ public static final String KEY_LOCKSCREEN_TARGETS = "lockscreen_targets";
     private ListPreference mCustomBackground;
     private CheckBoxPreference mSeeThrough;
     private ListPreference mStylePref;
+    private Preference mTextColor;
     private Preference mWeatherPref;
     private Preference mCalendarPref;
     private ListPreference mBatteryStatus;
@@ -94,6 +96,8 @@ public static final String KEY_LOCKSCREEN_TARGETS = "lockscreen_targets";
 
         mStylePref = (ListPreference) findPreference(KEY_STYLE_PREF);
         mStylePref.setOnPreferenceChangeListener(this);
+
+        mTextColor = (Preference) findPreference(KEY_TEXT_COLOR);
 
         wallpaperImage = new File(mActivity.getFilesDir()+"/lockwallpaper");
         wallpaperTemporary = new File(mActivity.getCacheDir()+"/lockwallpaper.tmp");
@@ -241,6 +245,14 @@ public static final String KEY_LOCKSCREEN_TARGETS = "lockscreen_targets";
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.LOCKSCREEN_SEE_THROUGH, value);
             return true;
+        } else if (preference == mTextColor) {
+            ColorPickerDialog cp = new ColorPickerDialog(getActivity(),
+                    mTextColorListener, Settings.System.getInt(getActivity()
+                    .getApplicationContext()
+                    .getContentResolver(), Settings.System.LOCKSCREEN_CUSTOM_TEXT_COLOR, 0xFFFFFFFF));
+            cp.setDefaultColor(0xFF000000);
+            cp.show();
+            return true;
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -353,4 +365,13 @@ public static final String KEY_LOCKSCREEN_TARGETS = "lockscreen_targets";
         }
         return false;
     }
+    ColorPickerDialog.OnColorChangedListener mTextColorListener =
+        new ColorPickerDialog.OnColorChangedListener() {
+            public void colorChanged(int color) {
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_CUSTOM_TEXT_COLOR, color);
+            }
+            public void colorUpdate(int color) {
+            }
+    };
 }

spezi77/android_packages_apps_Settings-2

Conflicts:
	res/values/strings.xml
	res/xml/lockscreen_interface_settings.xml
	src/com/android/settings/cyanogenmod/LockscreenInterface.java

src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -29,11 +29,13 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "LockscreenInterface";
     public static final String KEY_SEE_TRHOUGH_PREF = "lockscreen_see_through";
+    public static final String KEY_TEXT_COLOR = "lockscreen_custom_text_color";
     private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
     private static final String KEY_LOCKSCREEN_BUTTONS = "lockscreen_buttons";
 
     private PreferenceScreen mLockscreenButtons;
     private CheckBoxPreference mSeeThrough;
+    private Preference mTextColor;
     private ListPreference mBatteryStatus;
 
     public boolean hasButtons() {
@@ -49,6 +51,8 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         mSeeThrough = (CheckBoxPreference) findPreference(KEY_SEE_TRHOUGH_PREF);
         mSeeThrough.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 1));
+
+        mTextColor = (Preference) findPreference(KEY_TEXT_COLOR);
 
         // Battery status
         mBatteryStatus = (ListPreference) findPreference(KEY_ALWAYS_BATTERY_PREF);
@@ -73,6 +77,14 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.LOCKSCREEN_SEE_THROUGH, value);
             return true;
+        } else if (preference == mTextColor) {
+            ColorPickerDialog cp = new ColorPickerDialog(getActivity(),
+                    mTextColorListener, Settings.System.getInt(getActivity()
+                    .getApplicationContext()
+                    .getContentResolver(), Settings.System.LOCKSCREEN_CUSTOM_TEXT_COLOR, 0xFFFFFFFF));
+            cp.setDefaultColor(0xFFFFFFFF);
+            cp.show();
+            return true;
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -90,4 +102,13 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         }
         return false;
     }
+    ColorPickerDialog.OnColorChangedListener mTextColorListener =
+        new ColorPickerDialog.OnColorChangedListener() {
+            public void colorChanged(int color) {
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_CUSTOM_TEXT_COLOR, color);
+            }
+            public void colorUpdate(int color) {
+            }
+    };
 }