5878ac83ddc6f0500ac839760347d23f1f588f62
scotthartbti <scott.hart.bti@gmail.com>
2013-03-24 20:39:02 -0400

    Disable navigationbar unless navbar is enabled. Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Change-Id: I54eb191107439969bf1d0eadb65d44b440b74d27

--- src/com/android/settings/cyanogenmod/NavbarSettings.java
+++ src/com/android/settings/cyanogenmod/NavbarSettings.java
@@ -102,8 +102,6 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
         mEnableNavigationBar = (CheckBoxPreference) findPreference(ENABLE_NAVIGATION_BAR);
         mEnableNavigationBar.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.NAVIGATION_BAR_SHOW, mShowNavBar ? 1 : 0) == 1);
-        updateNavbarPreferences(Settings.System.getInt(getContentResolver(),
-                Settings.System.NAVIGATION_BAR_SHOW, mShowNavBar ? 1 : 0) == 1);
 
         // don't allow devices that must use a navigation bar to disable it
         if (hasNavBarByDefault) {
@@ -121,6 +119,8 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
                     Settings.System.NAVIGATION_BAR_CAN_MOVE, 1) == 0);
         }
 
+        updateNavbarPreferences(Settings.System.getInt(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_SHOW, mShowNavBar ? 1 : 0) == 1);
     }
 
     private void updateNavbarPreferences( boolean show ) {
@@ -130,7 +130,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
         mButtonPreference.setEnabled(show);
         mRingPreference.setEnabled(show);
         mStyleDimenPreference.setEnabled(show);
-
+	mNavigationBarCanMove.setEnabled(show);
     }
 
     @Override
