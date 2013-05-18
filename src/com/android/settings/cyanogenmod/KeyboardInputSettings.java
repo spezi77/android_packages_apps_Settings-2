4cb593845293a50c65bba444bcfe936049463a9d
blk_jack <jcorcoran+github@gmail.com>
2013-05-18 05:00:22 -0400

    Settings: Option to force keyboard to show enter key (2/2)
    Original SlimRoms feature!  Not everyone likes emoticons :-(
    
    Change-Id: I7dc8fd83ecf236ad5279b50ef916d08034c47882
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/keyboard_input_settings.xml

--- src/com/android/settings/cyanogenmod/KeyboardInputSettings.java
+++ src/com/android/settings/cyanogenmod/KeyboardInputSettings.java
@@ -24,12 +24,14 @@ public class KeyboardInputSettings extends SettingsPreferenceFragment implements
     private static final String PREF_DISABLE_FULLSCREEN_KEYBOARD = "disable_fullscreen_keyboard";
     private static final String KEYBOARD_ROTATION_TOGGLE = "keyboard_rotation_toggle";
     private static final String KEYBOARD_ROTATION_TIMEOUT = "keyboard_rotation_timeout";
+    private static final String SHOW_ENTER_KEY = "show_enter_key";
 
     private static final int KEYBOARD_ROTATION_TIMEOUT_DEFAULT = 5000; // 5s
 
     CheckBoxPreference mDisableFullscreenKeyboard;
     private CheckBoxPreference mKeyboardRotationToggle;
     private ListPreference mKeyboardRotationTimeout;
+    private CheckBoxPreference mShowEnterKey;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +51,10 @@ public class KeyboardInputSettings extends SettingsPreferenceFragment implements
         mKeyboardRotationTimeout.setOnPreferenceChangeListener(this);
         updateRotationTimeout(Settings.System.getInt(getActivity()
                     .getContentResolver(), Settings.System.KEYBOARD_ROTATION_TIMEOUT, KEYBOARD_ROTATION_TIMEOUT_DEFAULT));
+
+        mShowEnterKey = (CheckBoxPreference) findPreference(SHOW_ENTER_KEY);
+        mShowEnterKey.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.FORMAL_TEXT_INPUT, 0) == 1);
     }
 
     public void updateRotationTimeout(int timeout) {
@@ -94,6 +100,10 @@ public class KeyboardInputSettings extends SettingsPreferenceFragment implements
                     Settings.System.KEYBOARD_ROTATION_TIMEOUT,
                     mKeyboardRotationToggle.isChecked() ? KEYBOARD_ROTATION_TIMEOUT_DEFAULT : 0);
             updateRotationTimeout(KEYBOARD_ROTATION_TIMEOUT_DEFAULT);
+            return true;
+        } else if (preference == mShowEnterKey) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.FORMAL_TEXT_INPUT, mShowEnterKey.isChecked() ? 1 : 0);
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
