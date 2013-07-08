eaf6e7bbb7159e1121411c1b3723e4be2dcac880
scotthartbti <scott@ubuntu.com>
2013-07-07 21:16:34 -0400

    Power'full' Sleep 2/2 (credit slimrom) Signed-off-by: scotthartbti <scott@ubuntu.com>
    Change-Id: I6b0d4c47bd17bb1fbf5529ff49691d69ab7b42f1

--- src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
+++ src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
@@ -106,8 +106,8 @@ public class QuickSettingsUtil {
                 TILE_EXPANDED_DESKTOP, R.string.title_tile_expanded_desktop,
                 "com.android.systemui:drawable/ic_qs_expanded_desktop_off", true));
         TILES.put(TILE_SLEEP, new QuickSettingsUtil.TileInfo(
-                TILE_SLEEP, R.string.title_tile_sleep,
-                "com.android.systemui:drawable/ic_qs_sleep", true));
+                TILE_SLEEP, R.string.title_tile_power,
+                "com.android.systemui:drawable/ic_qs_power", true));
         TILES.put(TILE_GPS, new QuickSettingsUtil.TileInfo(
                 TILE_GPS, R.string.title_tile_gps,
                 "com.android.systemui:drawable/ic_qs_gps_neutral", true));
