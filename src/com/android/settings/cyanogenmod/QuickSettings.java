006fb94bd00de29af888543fe26aa5ac93947fcc
Evan Anderson <evan1124@gmail.com>
2013-07-30 00:54:20 -0500

    Quick Settings: Option to use floating window (1/2)
    Allows the option of launching preferences from the quicksettings menu in a
    floating window
    
    Change-Id: I267cae3cddaf8be697e5c60e21f7ba2616344a19
    Signed-off-by: Evan Anderson <evan1124@gmail.com>
    Signed-off-by: scott <scott@ubuntu.com>
    
    Conflicts:
    	res/values/pa_strings.xml
    	src/com/android/settings/cyanogenmod/QuickSettings.java

--- src/com/android/settings/cyanogenmod/QuickSettings.java
+++ src/com/android/settings/cyanogenmod/QuickSettings.java
@@ -65,6 +65,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
     private static final String GENERAL_SETTINGS = "pref_general_settings";
     private static final String STATIC_TILES = "static_tiles";
     private static final String DYNAMIC_TILES = "pref_dynamic_tiles";
+    private static final String FLOATING_WINDOW ="floating_window";
 
     MultiSelectListPreference mRingMode;
     ListPreference mNetworkMode;
@@ -73,6 +74,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
     CheckBoxPreference mDynamicBugReport;
     CheckBoxPreference mDynamicWifi;
     CheckBoxPreference mDynamicIme;
+    CheckBoxPreference mFloatingWindow;
     CheckBoxPreference mDynamicUsbTether;
     CheckBoxPreference mCollapsePanel;
     ListPreference mQuickPulldown;
@@ -119,6 +121,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
 
         mCollapsePanel = (CheckBoxPreference) prefSet.findPreference(COLLAPSE_PANEL);
         mCollapsePanel.setChecked(Settings.System.getInt(resolver, Settings.System.QS_COLLAPSE_PANEL, 0) == 1);
+
+	mFloatingWindow = (CheckBoxPreference) prefSet.findPreference(FLOATING_WINDOW);
+        mFloatingWindow.setChecked(Settings.System.getInt(resolver, Settings.System.QS_FLOATING_WINDOW, 0) == 1);
 
         // Add the sound mode
         mRingMode = (MultiSelectListPreference) prefSet.findPreference(EXP_RING_MODE);
@@ -195,6 +200,10 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
         } else if (preference == mCollapsePanel) {
             Settings.System.putInt(resolver, Settings.System.QS_COLLAPSE_PANEL,
                     mCollapsePanel.isChecked() ? 1 : 0);
+            return true;
+	} else if (preference == mFloatingWindow) {
+            Settings.System.putInt(resolver, Settings.System.QS_FLOATING_WINDOW,
+                    mFloatingWindow.isChecked() ? 1 : 0);
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
