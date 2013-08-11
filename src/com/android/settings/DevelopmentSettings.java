587dba2933d9201700bc41501fd7465a750ac696
scott <scott@ubuntu.com>
2013-08-11 03:17:20 -0400

    Revert "Revert "Make sure to not crash if DSP manager and/or development apps are""
    This reverts commit 10d174dbe88de8845b45221575f3fae277df9d68.

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -70,6 +70,8 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.android.settings.SettingsPreferenceFragment;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -77,7 +79,7 @@ import java.util.List;
 /*
  * Displays preferences for application developers.
  */
-public class DevelopmentSettings extends PreferenceFragment
+public class DevelopmentSettings extends SettingsPreferenceFragment
         implements DialogInterface.OnClickListener, DialogInterface.OnDismissListener,
                 OnPreferenceChangeListener, CompoundButton.OnCheckedChangeListener {
     private static final String TAG = "DevelopmentSettings";
@@ -353,7 +355,9 @@ public class DevelopmentSettings extends PreferenceFragment
         }
 
         mDevelopmentTools = (PreferenceScreen) findPreference(DEVELOPMENT_TOOLS);
-        mAllPrefs.add(mDevelopmentTools);
+        if (!removePreferenceIfPackageNotInstalled(mDevelopmentTools)) {
+            mAllPrefs.add(mDevelopmentTools);
+        }
     }
 
     private ListPreference addListPreference(String prefKey) {
