f0507bc4e70ccc35e1c0bc3717d82af3992113a7
scott <scott@ubuntu.com>
2013-09-02 01:19:00 -0400

    Revert "Added vibration adjustments via Performance" I thought i removed you a long time ago.
    This reverts commit 4f912ca0e754a68fafbc741f3bfea8bb5d252fad.
    
    Signed-off-by: scott <scott@ubuntu.com>
    
    Conflicts:
    	res/xml/performance_settings.xml
    	src/com/android/settings/cyanogenmod/BootReceiver.java
    	src/com/android/settings/cyanogenmod/PerformanceSettings.java
    
    Change-Id: I18381d22639afc3b607257c32bc65f83fabc8a58

--- src/com/android/settings/cyanogenmod/PerformanceSettings.java
+++ src/com/android/settings/cyanogenmod/PerformanceSettings.java
@@ -21,11 +21,9 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
-import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 
-import com.android.settings.Utils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -39,14 +37,9 @@ public class PerformanceSettings extends SettingsPreferenceFragment {
 
     private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
 
-    public static final String VIBE_STR = "pref_vibe_strength";
-    public static final String VIBE_STR_FILE = "/sys/class/timed_output/vibrator/vibe_strength"; 
-
     private CheckBoxPreference mUse16bppAlphaPref;
 
     private AlertDialog alertDialog;
-
-    private EditTextPreference mVibeStrength;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,16 +52,6 @@ public class PerformanceSettings extends SettingsPreferenceFragment {
             mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
             String use16bppAlpha = SystemProperties.get(USE_16BPP_ALPHA_PROP, "0");
             mUse16bppAlphaPref.setChecked("1".equals(use16bppAlpha));
-
-	    mVibeStrength = (EditTextPreference) prefSet.findPreference(VIBE_STR);
-            if (!Utils.fileExists(VIBE_STR_FILE)) {
-                prefSet.removePreference(mVibeStrength);
-            } else {
-                mVibeStrength.setOnPreferenceChangeListener(this);
-                String mCurVibeStrength = Utils.fileReadOneLine(VIBE_STR_FILE);
-                mVibeStrength.setSummary(getString(R.string.pref_vibe_strength_summary, mCurVibeStrength));
-                mVibeStrength.setText(mCurVibeStrength);
-            } 
     }
 
     @Override
@@ -81,22 +64,6 @@ public class PerformanceSettings extends SettingsPreferenceFragment {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
 
-        return true;
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mVibeStrength) {
-            int strength = Integer.parseInt((String) newValue);
-            if (strength > 120 || strength < 0) {
-                return false;
-            }
-            if (Utils.fileWriteOneLine(VIBE_STR_FILE, (String) newValue)) {
-                mVibeStrength.setSummary(getString(R.string.pref_vibe_strength_summary, (String) newValue));
-                return true;
-            } else {
-                return false;
-            } 
-        }
         return true;
     }
 
