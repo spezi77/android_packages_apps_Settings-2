eb2517f354e43f35b49dd32b2d2860663b9f7702
scott <scott@ubuntu>
2014-09-18 01:29:26 -0400

    missed this change that was part of the quiet hours rewrite.  oops.
    Change-Id: Ifc7bdaabb09f050b4ce34afc2bc90d561157234b
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -2,8 +2,12 @@
 package com.android.settings.cyanogenmod;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.provider.Settings;
 
 import com.android.settings.DisplaySettings;
 import com.android.settings.R;
@@ -18,6 +22,21 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context ctx, Intent intent) {
 
+	ContentResolver resolver = context.getContentResolver();
+
+ 	if (Settings.System.getInt(resolver,
+ 		Settings.System.QUIET_HOURS_REQUIRE_CHARGING, 0) != 0) {
+ 	    IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+ 	    Intent batteryStatus = context.registerReceiver(null, ifilter);
+ 	    final int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+ 	    final boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+ 	    	status == BatteryManager.BATTERY_STATUS_FULL;
+
+ 	    Settings.System.putInt(resolver,
+ 		Settings.System.QUIET_HOURS_REQUIRE_CHARGING, isCharging ? 2 : 1);
+ }
+
+	QuietHoursController.getInstance(ctx).scheduleService();
         /* Restore hardware tunable values */
         DisplayColor.restore(ctx);
         DisplayGamma.restore(ctx);
