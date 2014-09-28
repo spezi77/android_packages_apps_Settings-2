409e08f8b103aa8aad88105585ecdccba585028c
scott <scott@ubuntu>
2014-09-28 01:16:49 -0400

    Cleanup the settings layout
    Beanstalk interface settings and main settings screen is getting
    sloppy from all the different options and features that have been
    jammed in... introduce a new menu called advanced controls and move
    some of the higher end features into it so they are all centralized
    
    Change-Id: I32d0e2c24f95890ee9b7363aec4fc7913394bd6e
    Signed-off-by: scott <scott@ubuntu>

---
+++ src/com/android/settings/beanstalk/AppCircleBar.java
@@ -0,0 +1,157 @@
+package com.android.settings.beanstalk;
+
+import android.widget.Toast;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.content.ContentResolver;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.text.TextUtils;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+import com.android.settings.beanstalk.AppMultiSelectListPreference;
+import com.android.settings.beanstalk.SeekBarPreferenceChOS;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.lang.Thread;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class AppCircleBar extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_ENABLE_APP_CIRCLE_BAR = "enable_app_circle_bar";
+    private static final String PREF_INCLUDE_APP_CIRCLE_BAR_KEY = "app_circle_bar_included_apps";
+    private static final String KEY_TRIGGER_WIDTH = "trigger_width";
+    private static final String KEY_TRIGGER_TOP = "trigger_top";
+    private static final String KEY_TRIGGER_BOTTOM = "trigger_bottom";
+
+    private AppMultiSelectListPreference mIncludedAppCircleBar;
+    private CheckBoxPreference mEnableAppCircleBar;
+    private SeekBarPreferenceChOS mTriggerWidthPref;
+    private SeekBarPreferenceChOS mTriggerTopPref;
+    private SeekBarPreferenceChOS mTriggerBottomPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.app_circle_bar);
+
+	PreferenceScreen prefSet = getPreferenceScreen();
+
+	// App circle bar
+	mEnableAppCircleBar = (CheckBoxPreference) prefSet.findPreference(PREF_ENABLE_APP_CIRCLE_BAR);
+	mEnableAppCircleBar.setChecked((Settings.System.getInt(getContentResolver(),
+	Settings.System.ENABLE_APP_CIRCLE_BAR, 0) == 1));
+
+	mIncludedAppCircleBar = (AppMultiSelectListPreference) prefSet.findPreference		(PREF_INCLUDE_APP_CIRCLE_BAR_KEY);
+	Set<String> includedApps = getIncludedApps();
+	if (includedApps != null) mIncludedAppCircleBar.setValues(includedApps);
+	mIncludedAppCircleBar.setOnPreferenceChangeListener(this);
+
+	mTriggerWidthPref = (SeekBarPreferenceChOS) findPreference(KEY_TRIGGER_WIDTH);
+	mTriggerWidthPref.setValue(Settings.System.getInt(getContentResolver(),
+	Settings.System.APP_CIRCLE_BAR_TRIGGER_WIDTH, 10));
+	mTriggerWidthPref.setOnPreferenceChangeListener(this);
+
+	mTriggerTopPref = (SeekBarPreferenceChOS) findPreference(KEY_TRIGGER_TOP);
+	mTriggerTopPref.setValue(Settings.System.getInt(getContentResolver(),
+	Settings.System.APP_CIRCLE_BAR_TRIGGER_TOP, 0));
+	mTriggerTopPref.setOnPreferenceChangeListener(this);
+
+	mTriggerBottomPref = (SeekBarPreferenceChOS) findPreference(KEY_TRIGGER_BOTTOM);
+	mTriggerBottomPref.setValue(Settings.System.getInt(getContentResolver(),
+	Settings.System.APP_CIRCLE_BAR_TRIGGER_HEIGHT, 100));
+	mTriggerBottomPref.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onPause() {
+	super.onPause();
+	Settings.System.putInt(getContentResolver(),
+		Settings.System.APP_CIRCLE_BAR_SHOW_TRIGGER, 0);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+	Settings.System.putInt(getContentResolver(),
+		Settings.System.APP_CIRCLE_BAR_SHOW_TRIGGER, 1);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+	ContentResolver resolver = getActivity().getContentResolver();
+	boolean value;
+	if (preference == mEnableAppCircleBar) {
+	    boolean checked = ((CheckBoxPreference)preference).isChecked();
+	    Settings.System.putInt(resolver,
+		Settings.System.ENABLE_APP_CIRCLE_BAR, checked ? 1:0);
+	} else {
+	    return super.onPreferenceTreeClick(preferenceScreen, preference);
+	}
+
+	return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	final String key = preference.getKey();
+	if (preference == mTriggerWidthPref) {
+	    int width = ((Integer)newValue).intValue();
+	    Settings.System.putInt(getContentResolver(),
+		Settings.System.APP_CIRCLE_BAR_TRIGGER_WIDTH, width);
+	    return true;
+	}
+
+    	if (preference == mTriggerTopPref) {
+	    int top = ((Integer)newValue).intValue();
+	    Settings.System.putInt(getContentResolver(),
+		Settings.System.APP_CIRCLE_BAR_TRIGGER_TOP, top);
+	    return true;
+	}
+
+	if (preference == mTriggerBottomPref) {
+	    int bottom = ((Integer)newValue).intValue();
+	    Settings.System.putInt(getContentResolver(),
+		Settings.System.APP_CIRCLE_BAR_TRIGGER_HEIGHT, bottom);
+	    return true;
+	}
+	if (preference == mIncludedAppCircleBar) {
+	    storeIncludedApps((Set<String>) newValue);
+	}
+        return false;
+    }
+
+    private Set<String> getIncludedApps() {
+	String included = Settings.System.getString(getActivity().getContentResolver(),
+			Settings.System.WHITELIST_APP_CIRCLE_BAR);
+	if (TextUtils.isEmpty(included)) {
+		return null;
+	}
+	return new HashSet<String>(Arrays.asList(included.split("\\|")));
+    }
+
+    private void storeIncludedApps(Set<String> values) {
+	StringBuilder builder = new StringBuilder();
+	String delimiter = "";
+	for (String value : values) {
+		builder.append(delimiter);
+		builder.append(value);
+		delimiter = "|";
+	}
+	Settings.System.putString(getActivity().getContentResolver(),
+		Settings.System.WHITELIST_APP_CIRCLE_BAR, builder.toString());
+    }
+}
