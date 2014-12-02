1164061f3bb97ff85ffb7d5bcd2b3e76aa5041da
scott <scott@ubuntu>
2014-12-02 00:27:50 -0500

    Settings: Show clock enabled/disabled status summary
    Change-Id: I01195313e2eeabe66ea8bcd0e06d8680716b8899
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/beanstalk/StatusBarSettings.java
+++ src/com/android/settings/beanstalk/StatusBarSettings.java
@@ -17,19 +17,45 @@ import com.android.settings.Utils;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String TAG = "StatusBarSettings";
+    private static final String KEY_STATUS_BAR_CLOCK = "clock_style_pref";
+
+    private PreferenceScreen mClockStyle;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.beanstalk_statusbar_settings);
+
+	mClockStyle = (PreferenceScreen) prefSet.findPreference(KEY_STATUS_BAR_CLOCK);
+	updateClockStyleDescription();
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         return false;
     }
 
+    @Override
+    public void onResume() {
+	super.onResume();
+	updateClockStyleDescription();
+    }
+
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
  		return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void updateClockStyleDescription() {
+	if (mClockStyle == null) {
+	    return;
+	}
+	if (Settings.System.getInt(getContentResolver(),
+		Settings.System.STATUS_BAR_CLOCK, 1) == 1) {
+	    mClockStyle.setSummary(getString(R.string.enabled));
+	} else {
+	    mClockStyle.setSummary(getString(R.string.disabled));
+	}
     }
 }
