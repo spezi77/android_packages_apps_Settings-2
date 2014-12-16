f9751590a202424beffdd5a2a89ecadfe8d25b87
scott <scott@ubuntu>
2014-12-16 00:52:13 -0500

    Settings: StatusBar Ticker (2/2)
    Change-Id: Idb51639b3497d2761f5dc434da334f33a8379c33
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/beanstalk/StatusBarSettings.java
+++ src/com/android/settings/beanstalk/StatusBarSettings.java
@@ -7,8 +7,12 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -19,8 +23,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 
     private static final String TAG = "StatusBarSettings";
     private static final String KEY_STATUS_BAR_CLOCK = "clock_style_pref";
+    private static final String KEY_STATUS_BAR_TICKER = "status_bar_ticker_enabled";
 
     private PreferenceScreen mClockStyle;
+    private SwitchPreference mTicker;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,11 +36,33 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 
 	PreferenceScreen prefSet = getPreferenceScreen();
 
+	PackageManager pm = getPackageManager();
+	Resources systemUiResources;
+	try {
+	    systemUiResources = pm.getResourcesForApplication("com.android.systemui");
+	} catch (Exception e) {
+	    Log.e(TAG, "can't access systemui resources",e);
+	    return;
+	}
+
 	mClockStyle = (PreferenceScreen) prefSet.findPreference(KEY_STATUS_BAR_CLOCK);
 	updateClockStyleDescription();
+
+	mTicker = (SwitchPreference) prefSet.findPreference(KEY_STATUS_BAR_TICKER);
+	final boolean tickerEnabled = systemUiResources.getBoolean(systemUiResources.getIdentifier(
+		"com.android.systemui:bool/enable_ticker", null, null));
+	mTicker.setChecked(Settings.System.getInt(getContentResolver(),
+		Settings.System.STATUS_BAR_TICKER_ENABLED, tickerEnabled ? 1 : 0) == 1);
+	mTicker.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+	if (preference == mTicker) {
+	    Settings.System.putInt(getContentResolver(),
+		Settings.System.STATUS_BAR_TICKER_ENABLED,
+		(Boolean) objValue ? 1 : 0);
+	    return true;
+	}
         return false;
     }
 
