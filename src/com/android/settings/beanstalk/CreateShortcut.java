VanirAOSP/packages_apps_Settings

Squash of Dave's commits for Navi (2/2)
Inception to 1/4/15.  For testing

Vanir actions and utilities [2/2]

activities for vanir actions and utilities

Change-Id: Ie8c8faed8b1309442b71899b32f5acb31ccc228d
Signed-off-by: DHOMD <DHO@dho.im>
Conflicts:
	AndroidManifest.xml

Customizable navbar [2/2]

Change-Id: I3481687dbd7bc7de903ab9266a0473b33185f5ff
Signed-off-by: DHOMD <DHO@dho.im>

Conflicts:
	res/values/vanir_arrays.xml
	res/values/vanir_dimens.xml
	res/values/vanir_strings.xml

Cleanup

Tabs,whitespaces

Change-Id: I90488a92c80b764a91acc6eada8d91a2c223c9b8
Signed-off-by: DHOMD <DHO@dho.im>
Conflicts:
	res/values/vanir_arrays.xml
	res/values/vanir_dimens.xml
	res/values/vanir_strings.xml
	res/xml/vanir_interface.xml

ArrangeNavbarFragment: fix race condition

Change-Id: Ic854476db3478d31637ac1b1fb6860ef12c88b80

Navbar icons: since DocumentsUI is a megafail lets guide users to gallery

Change-Id: Ic9b179a24634ec7ca1d407a7f9ae2a581dd9d307
Signed-off-by: DHOMD <DHO@dho.im>

Conflicts:
	res/values/vanir_strings.xml

Navring toggle: make sure to use the correct default value

Change-Id: I641731882792bbf3eab7cc9c9e48556b470aaa39

src/com/android/settings/vanir/CreateShortcut.java
@@ -1,0 +1,90 @@
+/*
+* Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.settings.vanir;
+
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import com.android.settings.R;
+
+// Vanir actions and utilities setup
+
+public class CreateShortcut extends LauncherActivity {
+
+    @Override
+    protected Intent getTargetIntent() {
+        Intent targetIntent = new Intent(Intent.ACTION_MAIN, null);
+        targetIntent.addCategory("com.android.systemui.SHORTCUT");
+        targetIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return targetIntent;
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent shortcutIntent = intentForPosition(position);
+
+        String intentClass = shortcutIntent.getComponent().getClassName();
+        String intentAction = shortcutIntent.getAction();
+
+        shortcutIntent = new Intent();
+        shortcutIntent.setClassName(this, intentClass);
+        shortcutIntent.setAction(intentAction);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+        Intent.ShortcutIconResource.fromContext(this, getProperShortcutIcon(intentClass)));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, itemForPosition(position).label);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    private int getProperShortcutIcon(String className) {
+        String c = className.substring(className.lastIndexOf(".") + 1);
+
+        if (c.equals ("FlushMemory")) {
+            return R.drawable.vanir_flush_memory;
+
+        } else if (c.equals ("RingVibToggle")) {
+            return R.drawable.ic_lockscreen_vib;
+
+        } else if (c.equals ("RingSilentToggle")) {
+            return R.drawable.ic_lockscreen_silent;
+
+        } else if (c.equals ("RingVibSilentToggle")) {
+            return R.drawable.ic_lockscreen_soundon;
+
+        } else if (c.equals ("StartDreamActivity")) {
+            return R.drawable.ic_sysbar_dream;
+
+        } else if (c.equals ("ProfileActivity")) {
+            return R.drawable.ic_sysbar_profile;
+
+        } else {
+            return R.drawable.ic_sysbar_null;
+        }
+    }
+
+    @Override
+    protected boolean onEvaluateShowIcons() {
+        return false;
+    }
+}
+

src/com/android/settings/vanir/activities/RingVibSilentToggle.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+/*
+ * Toggle Ring/Vibrate/Silent
+ */
+
+public class RingVibSilentToggle extends Activity  {
+
+    public RingVibSilentToggle() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am != null) {
+            if (am.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+                am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vib != null) {
+                    vib.vibrate(50);
+                }
+            } else if (am.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE) {
+                am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, (int)(ToneGenerator.MAX_VOLUME * 0.85));
+                if (tg != null) {
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+            }
+        }
+    finish();
+    }
+}

src/com/android/settings/vanir/navbar/SettingsButtonInfo.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2014 VanirAOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.navbar;
+
+import android.text.TextUtils;
+
+public class SettingsButtonInfo {
+    String singleAction, doubleTapAction, longPressAction, iconUri;
+
+    public SettingsButtonInfo(String singleTap, String doubleTap, String longPress, String uri) {
+        this.singleAction = singleTap;
+        this.doubleTapAction = doubleTap;
+        this.longPressAction = longPress;
+        this.iconUri = uri;
+
+        if (singleAction == null) singleAction = "";
+        if (doubleTapAction == null) doubleTapAction = "";
+        if (longPressAction == null) longPressAction = "";
+        if (iconUri == null) iconUri = "";
+    }
+
+    @Override
+    public String toString() {
+        return singleAction + "," + doubleTapAction + "," + longPressAction + "," + iconUri;
+    }
+}

src/com/android/settings/vanir/util/SimpleFloatViewManager.java
@@ -1,0 +1,106 @@
+/**
+ * A subclass of the Android ListView component that enables drag
+ * and drop re-ordering of list items.
+ *
+ * Copyright 2012 Carl Bauer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.util;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Color;
+import android.widget.ListView;
+import android.widget.ImageView;
+import android.view.View;
+import android.util.Log;
+
+/**
+ * Simple implementation of the FloatViewManager class. Uses list
+ * items as they appear in the ListView to create the floating View.
+ */
+public class SimpleFloatViewManager implements DragSortListView.FloatViewManager {
+
+    private Bitmap mFloatBitmap;
+
+    private ImageView mImageView;
+
+    private int mFloatBGColor = Color.BLACK;
+
+    protected ListView mListView;
+
+    public SimpleFloatViewManager(ListView lv) {
+        mListView = lv;
+    }
+
+    public void setBackgroundColor(int color) {
+        mFloatBGColor = color;
+    }
+
+    /**
+     * This simple implementation creates a Bitmap copy of the
+     * list item currently shown at ListView <code>position</code>.
+     */
+    @Override
+    public View onCreateFloatView(int position) {
+        // Guaranteed that this will not be null? I think so. Nope, got
+        // a NullPointerException once...
+        View v = mListView.getChildAt(position + mListView.getHeaderViewsCount() - mListView.getFirstVisiblePosition());
+
+        if (v == null) {
+            return null;
+        }
+
+        v.setPressed(false);
+
+        // Create a copy of the drawing cache so that it does not get
+        // recycled by the framework when the list tries to clean up memory
+        //v.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        v.setDrawingCacheEnabled(true);
+        mFloatBitmap = Bitmap.createBitmap(v.getDrawingCache());
+        v.setDrawingCacheEnabled(false);
+
+        if (mImageView == null) {
+            mImageView = new ImageView(mListView.getContext());
+        }
+        mImageView.setBackgroundColor(mFloatBGColor);
+        mImageView.setPadding(0, 0, 0, 0);
+        mImageView.setImageBitmap(mFloatBitmap);
+
+        return mImageView;
+    }
+
+    /**
+     * This does nothing
+     */
+    @Override
+    public void onDragFloatView(View floatView, Point position, Point touch) {
+        // do nothing
+    }
+
+    /**
+     * Removes the Bitmap from the ImageView created in
+     * onCreateFloatView() and tells the system to recycle it.
+     */
+    @Override
+    public void onDestroyFloatView(View floatView) {
+        ((ImageView) floatView).setImageDrawable(null);
+
+        mFloatBitmap.recycle();
+        mFloatBitmap = null;
+    }
+
+}
+

src/com/android/settings/vanir/widgets/CategorySeparator.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2013 AOKP
+ * Copyright (C) 2014 VanirAOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.android.settings.R;
+import com.android.settings.vanir.BaseSetting;
+
+public class CategorySeparator extends LinearLayout {
+
+    public CategorySeparator(Context context) {
+        this(context, null);
+    }
+
+    public CategorySeparator(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CategorySeparator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        View.inflate(context, R.layout.category_separator, this);
+
+        if (attrs != null) {
+            int title = attrs.getAttributeResourceValue(BaseSetting.NAMESPACE_ANDROID, "title", 0);
+            if (title > 0) {
+                TextView textView = (TextView) findViewById(R.id.title);
+                textView.setText(title);
+                textView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+}

spezi77/android_packages_apps_Settings-2

Signed-off-by: scott <scott@ubuntu>
	res/xml/dashboard_categories.xml
	res/layout/ga_create_gesture.xml
	res/layout/ga_gestures_list.xml
	src/com/android/settings/beanstalk/gestureanywhere/GestureAnywhereCreateGestureActivity.java

src/com/android/settings/beanstalk/CreateShortcut.java
@@ -1,0 +1,90 @@
+/*
+* Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.settings.beanstalk;
+
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import com.android.settings.R;
+
+// Vanir actions and utilities setup
+
+public class CreateShortcut extends LauncherActivity {
+
+    @Override
+    protected Intent getTargetIntent() {
+        Intent targetIntent = new Intent(Intent.ACTION_MAIN, null);
+        targetIntent.addCategory("com.android.systemui.SHORTCUT");
+        targetIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return targetIntent;
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent shortcutIntent = intentForPosition(position);
+
+        String intentClass = shortcutIntent.getComponent().getClassName();
+        String intentAction = shortcutIntent.getAction();
+
+        shortcutIntent = new Intent();
+        shortcutIntent.setClassName(this, intentClass);
+        shortcutIntent.setAction(intentAction);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+        Intent.ShortcutIconResource.fromContext(this, getProperShortcutIcon(intentClass)));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, itemForPosition(position).label);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    private int getProperShortcutIcon(String className) {
+        String c = className.substring(className.lastIndexOf(".") + 1);
+
+        if (c.equals ("FlushMemory")) {
+            return R.drawable.vanir_flush_memory;
+
+        } else if (c.equals ("RingVibToggle")) {
+            return R.drawable.ic_lockscreen_vib;
+
+        } else if (c.equals ("RingSilentToggle")) {
+            return R.drawable.ic_lockscreen_silent;
+
+        } else if (c.equals ("RingVibSilentToggle")) {
+            return R.drawable.ic_lockscreen_soundon;
+
+        } else if (c.equals ("StartDreamActivity")) {
+            return R.drawable.ic_sysbar_dream;
+
+        } else if (c.equals ("ProfileActivity")) {
+            return R.drawable.ic_sysbar_profile;
+
+        } else {
+            return R.drawable.ic_sysbar_null;
+        }
+    }
+
+    @Override
+    protected boolean onEvaluateShowIcons() {
+        return false;
+    }
+}
+

src/com/android/settings/beanstalk/activities/RingVibSilentToggle.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+/*
+ * Toggle Ring/Vibrate/Silent
+ */
+
+public class RingVibSilentToggle extends Activity  {
+
+    public RingVibSilentToggle() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am != null) {
+            if (am.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+                am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vib != null) {
+                    vib.vibrate(50);
+                }
+            } else if (am.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE) {
+                am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, (int)(ToneGenerator.MAX_VOLUME * 0.85));
+                if (tg != null) {
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+            }
+        }
+    finish();
+    }
+}

src/com/android/settings/beanstalk/navbar/SettingsButtonInfo.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2014 VanirAOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk.navbar;
+
+import android.text.TextUtils;
+
+public class SettingsButtonInfo {
+    String singleAction, doubleTapAction, longPressAction, iconUri;
+
+    public SettingsButtonInfo(String singleTap, String doubleTap, String longPress, String uri) {
+        this.singleAction = singleTap;
+        this.doubleTapAction = doubleTap;
+        this.longPressAction = longPress;
+        this.iconUri = uri;
+
+        if (singleAction == null) singleAction = "";
+        if (doubleTapAction == null) doubleTapAction = "";
+        if (longPressAction == null) longPressAction = "";
+        if (iconUri == null) iconUri = "";
+    }
+
+    @Override
+    public String toString() {
+        return singleAction + "," + doubleTapAction + "," + longPressAction + "," + iconUri;
+    }
+}

src/com/android/settings/beanstalk/util/SimpleFloatViewManager.java
@@ -1,0 +1,106 @@
+/**
+ * A subclass of the Android ListView component that enables drag
+ * and drop re-ordering of list items.
+ *
+ * Copyright 2012 Carl Bauer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk.util;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Color;
+import android.widget.ListView;
+import android.widget.ImageView;
+import android.view.View;
+import android.util.Log;
+
+/**
+ * Simple implementation of the FloatViewManager class. Uses list
+ * items as they appear in the ListView to create the floating View.
+ */
+public class SimpleFloatViewManager implements DragSortListView.FloatViewManager {
+
+    private Bitmap mFloatBitmap;
+
+    private ImageView mImageView;
+
+    private int mFloatBGColor = Color.BLACK;
+
+    protected ListView mListView;
+
+    public SimpleFloatViewManager(ListView lv) {
+        mListView = lv;
+    }
+
+    public void setBackgroundColor(int color) {
+        mFloatBGColor = color;
+    }
+
+    /**
+     * This simple implementation creates a Bitmap copy of the
+     * list item currently shown at ListView <code>position</code>.
+     */
+    @Override
+    public View onCreateFloatView(int position) {
+        // Guaranteed that this will not be null? I think so. Nope, got
+        // a NullPointerException once...
+        View v = mListView.getChildAt(position + mListView.getHeaderViewsCount() - mListView.getFirstVisiblePosition());
+
+        if (v == null) {
+            return null;
+        }
+
+        v.setPressed(false);
+
+        // Create a copy of the drawing cache so that it does not get
+        // recycled by the framework when the list tries to clean up memory
+        //v.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        v.setDrawingCacheEnabled(true);
+        mFloatBitmap = Bitmap.createBitmap(v.getDrawingCache());
+        v.setDrawingCacheEnabled(false);
+
+        if (mImageView == null) {
+            mImageView = new ImageView(mListView.getContext());
+        }
+        mImageView.setBackgroundColor(mFloatBGColor);
+        mImageView.setPadding(0, 0, 0, 0);
+        mImageView.setImageBitmap(mFloatBitmap);
+
+        return mImageView;
+    }
+
+    /**
+     * This does nothing
+     */
+    @Override
+    public void onDragFloatView(View floatView, Point position, Point touch) {
+        // do nothing
+    }
+
+    /**
+     * Removes the Bitmap from the ImageView created in
+     * onCreateFloatView() and tells the system to recycle it.
+     */
+    @Override
+    public void onDestroyFloatView(View floatView) {
+        ((ImageView) floatView).setImageDrawable(null);
+
+        mFloatBitmap.recycle();
+        mFloatBitmap = null;
+    }
+
+}
+

src/com/android/settings/beanstalk/widgets/CategorySeparator.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2013 AOKP
+ * Copyright (C) 2014 VanirAOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.android.settings.R;
+import com.android.settings.beanstalk.BaseSetting;
+
+public class CategorySeparator extends LinearLayout {
+
+    public CategorySeparator(Context context) {
+        this(context, null);
+    }
+
+    public CategorySeparator(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CategorySeparator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        View.inflate(context, R.layout.category_separator, this);
+
+        if (attrs != null) {
+            int title = attrs.getAttributeResourceValue(BaseSetting.NAMESPACE_ANDROID, "title", 0);
+            if (title > 0) {
+                TextView textView = (TextView) findViewById(R.id.title);
+                textView.setText(title);
+                textView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+}