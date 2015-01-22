sooti/android_packages_apps_Settings

Settings: SlimPie (2/2) Implement SlimPie for lp5.0 ported from kitkat.
WIP
Still need to bring back a few more SlimPie features.

Signed-off-by: Dragon <dragon7780@hotmail.nl>

src/com/android/settings/fusion/PieControl.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2014 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fusion;
+
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.SwitchPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PieControl extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String PIE_CONTROL = "pie_control";
+    private static final String PIE_MENU = "pie_menu";
+
+    private SwitchPreference mPieControl;
+    private ListPreference mPieMenuDisplay;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pie_control);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mPieControl = (SwitchPreference) prefSet.findPreference(PIE_CONTROL);
+        mPieControl.setOnPreferenceChangeListener(this);
+
+        mPieMenuDisplay = (ListPreference) prefSet.findPreference(PIE_MENU);
+        mPieMenuDisplay.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPieControl) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_CONTROLS, (Boolean) newValue ? 1 : 0);
+        } else if (preference == mPieMenuDisplay) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_MENU, Integer.parseInt((String) newValue));
+        }
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mPieMenuDisplay.setValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.PIE_MENU,
+                2) + "");
+        mPieControl.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.PIE_CONTROLS, 0) == 1);
+    }
+}

src/com/android/settings/fusion/PieTriggerSettings.java
@@ -1,0 +1,113 @@
+/*
+ * Copyright (C) 2014 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fusion;
+
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PieTriggerSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    // This equals EdgeGesturePosition.LEFT.FLAG
+    private static final int DEFAULT_POSITION = 1 << 0;
+
+    private static final String[] TRIGGER = {
+        "pie_control_trigger_left",
+        "pie_control_trigger_bottom",
+        "pie_control_trigger_right",
+        "pie_control_trigger_top"
+    };
+
+    private SwitchPreference[] mTrigger = new SwitchPreference[4];
+
+    private ContentObserver mPieTriggerObserver = new ContentObserver(new Handler()) {
+        @Override
+        public void onChange(boolean selfChange) {
+            updatePieTriggers();
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pie_trigger);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        for (int i = 0; i < TRIGGER.length; i++) {
+            mTrigger[i] = (SwitchPreference) prefSet.findPreference(TRIGGER[i]);
+            mTrigger[i].setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        int triggerSlots = 0;
+        for (int i = 0; i < mTrigger.length; i++) {
+            boolean checked = preference == mTrigger[i]
+                    ? (Boolean) newValue : mTrigger[i].isChecked();
+            if (checked) {
+                triggerSlots |= 1 << i;
+            }
+        }
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.PIE_GRAVITY, triggerSlots);
+        updatePieTriggers();
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.PIE_GRAVITY), true,
+                mPieTriggerObserver);
+
+        updatePieTriggers();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getContentResolver().unregisterContentObserver(mPieTriggerObserver);
+    }
+
+    private void updatePieTriggers() {
+        int triggerSlots = Settings.System.getInt(getContentResolver(),
+                Settings.System.PIE_GRAVITY, DEFAULT_POSITION);
+
+        for (int i = 0; i < mTrigger.length; i++) {
+            if ((triggerSlots & (0x01 << i)) != 0) {
+                mTrigger[i].setChecked(true);
+            } else {
+                mTrigger[i].setChecked(false);
+            }
+        }
+    }
+}

src/com/android/settings/fusion/fragments/PieButtonFragment.java
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2014 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fusion.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PieButtonFragment extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pie_button_fragment);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+}

spezi77/android_packages_apps_Settings-2

Signed-off-by: scott <scott@beanstalk.com>
Conflicts:
	res/values/fusion_strings.xml
	res/xml/fusion_navigation_settings.xml
	src/com/android/settings/slim/dslv/ActionListViewSettings.java
Change-Id: Ie1ecba6549bf4f1dfaa2e134244f5386f459a77d

src/com/android/settings/beanstalk/PieControl.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2014 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk;
+
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.SwitchPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PieControl extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String PIE_CONTROL = "pie_control";
+    private static final String PIE_MENU = "pie_menu";
+
+    private SwitchPreference mPieControl;
+    private ListPreference mPieMenuDisplay;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pie_control);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mPieControl = (SwitchPreference) prefSet.findPreference(PIE_CONTROL);
+        mPieControl.setOnPreferenceChangeListener(this);
+
+        mPieMenuDisplay = (ListPreference) prefSet.findPreference(PIE_MENU);
+        mPieMenuDisplay.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPieControl) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_CONTROLS, (Boolean) newValue ? 1 : 0);
+        } else if (preference == mPieMenuDisplay) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_MENU, Integer.parseInt((String) newValue));
+        }
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mPieMenuDisplay.setValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.PIE_MENU,
+                2) + "");
+        mPieControl.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.PIE_CONTROLS, 0) == 1);
+    }
+}

src/com/android/settings/beanstalk/PieTriggerSettings.java
@@ -1,0 +1,113 @@
+/*
+ * Copyright (C) 2014 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk;
+
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PieTriggerSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    // This equals EdgeGesturePosition.LEFT.FLAG
+    private static final int DEFAULT_POSITION = 1 << 0;
+
+    private static final String[] TRIGGER = {
+        "pie_control_trigger_left",
+        "pie_control_trigger_bottom",
+        "pie_control_trigger_right",
+        "pie_control_trigger_top"
+    };
+
+    private SwitchPreference[] mTrigger = new SwitchPreference[4];
+
+    private ContentObserver mPieTriggerObserver = new ContentObserver(new Handler()) {
+        @Override
+        public void onChange(boolean selfChange) {
+            updatePieTriggers();
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pie_trigger);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        for (int i = 0; i < TRIGGER.length; i++) {
+            mTrigger[i] = (SwitchPreference) prefSet.findPreference(TRIGGER[i]);
+            mTrigger[i].setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        int triggerSlots = 0;
+        for (int i = 0; i < mTrigger.length; i++) {
+            boolean checked = preference == mTrigger[i]
+                    ? (Boolean) newValue : mTrigger[i].isChecked();
+            if (checked) {
+                triggerSlots |= 1 << i;
+            }
+        }
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.PIE_GRAVITY, triggerSlots);
+        updatePieTriggers();
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.PIE_GRAVITY), true,
+                mPieTriggerObserver);
+
+        updatePieTriggers();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getContentResolver().unregisterContentObserver(mPieTriggerObserver);
+    }
+
+    private void updatePieTriggers() {
+        int triggerSlots = Settings.System.getInt(getContentResolver(),
+                Settings.System.PIE_GRAVITY, DEFAULT_POSITION);
+
+        for (int i = 0; i < mTrigger.length; i++) {
+            if ((triggerSlots & (0x01 << i)) != 0) {
+                mTrigger[i].setChecked(true);
+            } else {
+                mTrigger[i].setChecked(false);
+            }
+        }
+    }
+}

src/com/android/settings/beanstalk/fragments/PieButtonFragment.java
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2014 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PieButtonFragment extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pie_button_fragment);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+}
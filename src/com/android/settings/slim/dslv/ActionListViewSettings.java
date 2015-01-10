d50c5e805abc79027b624718ac0c255b62aed454
Dragon <dragon7780@hotmail.nl>
2015-01-10 21:54:52 +0100

    Settings: SlimPie (2/2) Implement SlimPie for lp5.0 ported from kitkat.
    WIP
    Still need to bring back a few more SlimPie features.
    
    Signed-off-by: Dragon <dragon7780@hotmail.nl>
    Signed-off-by: scott <scott@beanstalk.com>
    
    Conflicts:
    	res/values/fusion_strings.xml
    	res/xml/fusion_navigation_settings.xml
    	src/com/android/settings/slim/dslv/ActionListViewSettings.java
    
    Change-Id: Ie1ecba6549bf4f1dfaa2e134244f5386f459a77d

--- src/com/android/settings/slim/dslv/ActionListViewSettings.java
+++ src/com/android/settings/slim/dslv/ActionListViewSettings.java
@@ -523,22 +523,21 @@ public class ActionListViewSettings extends ListFragment implements
                     mActivity, mActionValuesKey, mActionEntriesKey);
             case NAV_RING:
                 return ActionHelper.getNavRingConfigWithDescription(
-                    mActivity, mActionValuesKey, mActionEntriesKey);
+                    mActivity, mActionValuesKey, mActionEntriesKey);*/
             case PIE:
                 return ActionHelper.getPieConfigWithDescription(
                     mActivity, mActionValuesKey, mActionEntriesKey);
             case PIE_SECOND:
                 return ActionHelper.getPieSecondLayerConfigWithDescription(
                     mActivity, mActionValuesKey, mActionEntriesKey);
-            case POWER_MENU_SHORTCUT:
+/*            case POWER_MENU_SHORTCUT:
                 return PolicyHelper.getPowerMenuConfigWithDescription(
                     mActivity, mActionValuesKey, mActionEntriesKey);
             case LOCKSCREEN_SHORTCUT:
                 return ActionHelper.getLockscreenShortcutConfig(mActivity);
             case SHAKE_EVENTS_DISABLED:
-                return ActionHelper.getDisabledShakeApps(mActivity);
+                return ActionHelper.getDisabledShakeApps(mActivity);*/
         }
-*/
         return null;
     }
 
@@ -550,14 +549,14 @@ public class ActionListViewSettings extends ListFragment implements
                 break;
             case NAV_RING:
                 ActionHelper.setNavRingConfig(mActivity, actionConfigs, reset);
-                break;
+                break;*/
             case PIE:
                 ActionHelper.setPieConfig(mActivity, actionConfigs, reset);
                 break;
             case PIE_SECOND:
                 ActionHelper.setPieSecondLayerConfig(mActivity, actionConfigs, reset);
                 break;
-            case POWER_MENU_SHORTCUT:
+/*            case POWER_MENU_SHORTCUT:
                 PolicyHelper.setPowerMenuConfig(mActivity, actionConfigs, reset);
                 break;
             case LOCKSCREEN_SHORTCUT:
@@ -565,9 +564,8 @@ public class ActionListViewSettings extends ListFragment implements
                 break;
             case SHAKE_EVENTS_DISABLED:
                 ActionHelper.setDisabledShakeApps(mActivity, actionConfigs, reset);
-                break;
+                break;*/
         }
-*/
     }
 
     private class ViewHolder {
