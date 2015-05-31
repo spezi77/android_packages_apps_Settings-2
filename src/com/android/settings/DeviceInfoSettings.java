1b579d2e275b7b53de254960acc87c52d02240cd
noname55804 <marlin2201@gmail.com>
2015-05-31 13:30:50 +0200

    Saberize V5 (5/5)
    This is the fifth part of the Saberization, you can find the other parts in these repos:
    vendor, build, device, kernel, settings
    
    This patch enables an information about toolchain and optimization in the "About" section
    
    Credits: Paul Beeler
    
    Change-Id: Ia479dffc0ad4b1128ab51126f6970ee7979fb2b1
    Signed-off-by: noname55804 <marlin2201@gmail.com>
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	res/values/cm_bools.xml

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -91,6 +91,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_MOD_BUILD_DATE = "build_date";
     private static final String KEY_CM_UPDATES = "cm_updates";
     private static final String KEY_CM_LICENSE = "cmlicense";
+    private static final String KEY_SM_AND = "sm_android";
+    private static final String KEY_SM_FLAGS = "sm_flags";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -118,7 +120,13 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_VERSION, "ro.cm.display.version");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
+        setStringSummary(KEY_KERNEL_VERSION, getFormattedKernelVersion());
+	findPreference(KEY_KERNEL_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        findPreference(KEY_SM_AND).setEnabled(true);
+	findPreference(KEY_SM_FLAGS).setEnabled(true);
+	setValueSummary(KEY_SM_AND, "ro.sm.android");
+	setValueSummary(KEY_SM_FLAGS, "ro.sm.flags");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
@@ -224,7 +232,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference.getKey().equals(KEY_FIRMWARE_VERSION)) {
+        String prefKey = preference.getKey();
+        if (prefKey.equals(KEY_FIRMWARE_VERSION)) {
             System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
             mHits[mHits.length-1] = SystemClock.uptimeMillis();
             if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
@@ -305,7 +314,10 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             } catch (Exception e) {
                 Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
             }
-        }
+        } else if (prefKey.equals(KEY_KERNEL_VERSION)) {
+            setStringSummary(KEY_KERNEL_VERSION, getKernelVersion());
+            return true;
+	}
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
@@ -374,6 +386,20 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             reader.close();
         }
     }
+
+    private String getKernelVersion() {
+    String procVersionStr;
+    try {
+        procVersionStr = readLine(FILENAME_PROC_VERSION);
+        return procVersionStr;
+    	} catch (IOException e) {
+        Log.e(LOG_TAG,
+            "IO Exception when getting kernel version for Device Info screen",
+            e);
+
+       return "Unavailable";
+    	}
+    }	
 
     public static String getFormattedKernelVersion() {
         try {
