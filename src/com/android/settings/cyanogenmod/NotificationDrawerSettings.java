74d73718ce5623df8e1a31af404acaad93d87d25
Lars Greiss <kufikugel@googlemail.com>
2015-09-24 13:37:11 -0300

    Return: Smart Pulldown [2/2]
    Reworked for BS by spezi77
    
    Change-Id: Ib599f6ab7995034de5c6faa61641dd61cfd958a3
    Signed-off-by: spezi77 <spezi77@gmx.com>
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	res/values/bs_arrays.xml
    	res/values/bs_strings.xml
    	res/xml/status_bar_settings.xml

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -39,10 +39,12 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  impl
 
     private static final String PREF_CUSTOM_HEADER = "status_bar_custom_header";
     private static final String PREF_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
+    private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
 
     private SwitchPreference mCustomHeader;
     private SwitchPreference mCustomHeaderDefault;
     private SwitchPreference mEnableTaskManager;
+    private ListPreference mSmartPulldown;
     
     @Override
     public void onCreate(Bundle icicle) {
@@ -65,6 +67,14 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  impl
 	mEnableTaskManager = (SwitchPreference) findPreference(ENABLE_TASK_MANAGER);
         mEnableTaskManager.setChecked((Settings.System.getInt(resolver,
                 Settings.System.ENABLE_TASK_MANAGER, 0) == 1));
+
+        // Smart Pulldown
+        mSmartPulldown = (ListPreference) findPreference(PREF_SMART_PULLDOWN);
+        mSmartPulldown.setOnPreferenceChangeListener(this);
+        int smartPulldown = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_SMART_PULLDOWN, 0);
+        mSmartPulldown.setValue(String.valueOf(smartPulldown));
+        updateSmartPulldownSummary(smartPulldown);
 
     }
 
@@ -97,16 +107,45 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  impl
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT,
                     (Boolean) newValue ? 1 : 0);
             return true;
+        } else if (preference == mSmartPulldown) {
+            int smartPulldown = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.QS_SMART_PULLDOWN,
+                    smartPulldown);
+            updateSmartPulldownSummary(smartPulldown);
+            return true;
 	}
 
         return false;
     }
-
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
+    private void updateSmartPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Smart pulldown deactivated
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
+        } else {
+            String type = null;
+            switch (value) {
+                case 1:
+                    type = res.getString(R.string.smart_pulldown_dismissable);
+                    break;
+                case 2:
+                    type = res.getString(R.string.smart_pulldown_persistent);
+                    break;
+                default:
+                    type = res.getString(R.string.smart_pulldown_all);
+                    break;
+            }
+            // Remove title capitalized formatting
+            type = type.toLowerCase();
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
+        }
+    }
 
 }
