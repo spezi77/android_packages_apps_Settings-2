05ea62596136418047f953a888a941912ef0cf75
scott <scott@ubuntu>
2015-11-26 00:26:24 -0500

    Add option to disable scrolling cache [2/2]
    scrolling cache can cause performance lag due to GC
    allow user to enable/disable/force scrolling cache on the fly
    via Settings > CM > Performance > Scrolling cache
    see the framework part at http://review.cyanogenmod.com/#change,12302
    
    Original commit from way back in 2012
    nadlabak/android_packages_apps_CMParts@2c10284
    
    Change-Id: Idfdc1ee9ff38c10dfbe9245a19ec30d5b9603774
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -107,12 +107,16 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
     private static final String KEY_DISPLAY_ROTATION = "display_rotation";
     private static final String KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
+    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
+    private static final String SCROLLINGCACHE_DEFAULT = "1";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
     private ListPreference mLcdDensityPreference;
     private FontDialogPreference mFontSizePref;
     private PreferenceScreen mDisplayRotationPreference;
+    private ListPreference mScrollingCachePref;
 
     private final Configuration mCurConfig = new Configuration();
 
@@ -257,6 +261,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         } else {
             removePreference(KEY_CAMERA_DOUBLE_TAP_POWER_GESTURE);
         }
+
+	mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
+        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
+                SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
+        mScrollingCachePref.setOnPreferenceChangeListener(this);
 
         mNightModePreference = (ListPreference) findPreference(KEY_NIGHT_MODE);
         if (mNightModePreference != null) {
@@ -687,6 +696,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED,
                     value ? 0 : 1 /* Backwards because setting is for disabling */);
+        }
+	if (preference == mScrollingCachePref) {
+            if (objValue != null) {
+                SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, (String)objValue);
+                return true;
+            }
         }
         if (preference == mNightModePreference) {
             try {
