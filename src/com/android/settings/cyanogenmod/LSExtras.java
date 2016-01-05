fb20cd7337d941f503bc9059c4f3830d2833a215
scott <scott@ubuntu>
2016-01-04 22:46:20 -0500

    Lockscreen: Move extra lockscreen settings into separate menu to avoid clutter
    Also add lockscreen blur taken from https://github.com/AICP/packages_apps_AicpExtras/commit/b4aeb9a87bd5bbfc609f8f607331365339fe994e
    
    Change-Id: I19448ddcad332a7c1946e2b7fec5fe513b016788
    Signed-off-by: scott <scott@ubuntu>

---
+++ src/com/android/settings/cyanogenmod/LSExtras.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2016 BeanStalk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.cyanogenmod;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.util.Helpers;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import com.android.settings.widget.SeekBarPreferenceCham;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class LSExtras extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener, Indexable {
+
+    private static final String TAG = "LSExtras";
+
+    private static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
+    private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+    private static final String KEY_LOCKSCREEN_BLUR_RADIUS = "lockscreen_blur_radius";
+
+    private SeekBarPreferenceCham mMaxKeyguardNotifConfig;
+    private ListPreference mLockClockFonts;
+    private SeekBarPreferenceCham mBlurRadius;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.ls_extras);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+	mMaxKeyguardNotifConfig = (SeekBarPreferenceCham) findPreference(LOCKSCREEN_MAX_NOTIF_CONFIG);
+        int kgconf = Settings.System.getInt(mResolver,
+                Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 5);
+        mMaxKeyguardNotifConfig.setValue(kgconf);
+        mMaxKeyguardNotifConfig.setOnPreferenceChangeListener(this);
+
+	mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
+        mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
+                mResolver, Settings.System.LOCK_CLOCK_FONTS, 0)));
+        mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+        mLockClockFonts.setOnPreferenceChangeListener(this);
+
+	mBlurRadius = (SeekBarPreferenceCham) findPreference(KEY_LOCKSCREEN_BLUR_RADIUS);
+        mBlurRadius.setValue(Settings.System.getInt(resolver,
+                Settings.System.LOCKSCREEN_BLUR_RADIUS, 14));
+        mBlurRadius.setOnPreferenceChangeListener(this);
+        
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        // todo add a constant in MetricsLogger.java
+        return MetricsLogger.MAIN_SETTINGS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+	if (preference == mLockClockFonts) {
+            Settings.System.putInt(mResolver, Settings.System.LOCK_CLOCK_FONTS,
+                    Integer.valueOf((String) newValue));
+            mLockClockFonts.setValue(String.valueOf(newValue));
+            mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+            return true;
+	} else if (preference == mMaxKeyguardNotifConfig) {
+            int kgconf = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, kgconf);
+            return true;
+	} else if (preference == mBlurRadius) {
+            int width = ((Integer)newValue).intValue();
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCKSCREEN_BLUR_RADIUS, width);
+            return true;
+        }
+        return false;
+    }
+}
--- src/com/android/settings/cyanogenmod/Weather.java
+++ src/com/android/settings/cyanogenmod/Weather.java
@@ -50,8 +50,6 @@ public class Weather extends SettingsPreferenceFragment implements
     private static final String PREF_COLORIZE_ALL_ICONS = "weather_colorize_all_icons";
     private static final String PREF_TEXT_COLOR = "weather_text_color";
     private static final String PREF_ICON_COLOR = "weather_icon_color";
-    private static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
-    private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
 
     private static final int MONOCHROME_ICON = 0;
     private static final int DEFAULT_COLOR = 0xffffffff;
@@ -65,8 +63,6 @@ public class Weather extends SettingsPreferenceFragment implements
     private SwitchPreference mColorizeAllIcons;
     private ColorPickerPreference mTextColor;
     private ColorPickerPreference mIconColor;
-    private SeekBarPreferenceCham mMaxKeyguardNotifConfig;
-    private ListPreference mLockClockFonts;
 
     private ContentResolver mResolver;
 
@@ -107,18 +103,6 @@ public class Weather extends SettingsPreferenceFragment implements
         PreferenceCategory catColors = (PreferenceCategory) findPreference(PREF_CAT_COLORS);
         mTextColor = (ColorPickerPreference) findPreference(PREF_TEXT_COLOR);
         mIconColor = (ColorPickerPreference) findPreference(PREF_ICON_COLOR);
-
-	mMaxKeyguardNotifConfig = (SeekBarPreferenceCham) findPreference(LOCKSCREEN_MAX_NOTIF_CONFIG);
-        int kgconf = Settings.System.getInt(mResolver,
-                Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 5);
-        mMaxKeyguardNotifConfig.setValue(kgconf);
-        mMaxKeyguardNotifConfig.setOnPreferenceChangeListener(this);
-
-	mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
-        mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
-                mResolver, Settings.System.LOCK_CLOCK_FONTS, 0)));
-        mLockClockFonts.setSummary(mLockClockFonts.getEntry());
-        mLockClockFonts.setOnPreferenceChangeListener(this);
 
         if (showWeather) {
             mShowLocation = (SwitchPreference) findPreference(PREF_SHOW_LOCATION);
@@ -189,7 +173,6 @@ public class Weather extends SettingsPreferenceFragment implements
         boolean value;
         String hex;
         int intHex;
-
         if (preference == mShowWeather) {
             value = (Boolean) newValue;
             Settings.System.putInt(mResolver,
@@ -226,11 +209,6 @@ public class Weather extends SettingsPreferenceFragment implements
                     Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR, intHex);
             preference.setSummary(hex);
             return true;
-	} else if (preference == mMaxKeyguardNotifConfig) {
-            int kgconf = (Integer) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, kgconf);
-            return true;
         } else if (preference == mIconColor) {
             hex = ColorPickerPreference.convertToARGB(
                 Integer.valueOf(String.valueOf(newValue)));
@@ -238,12 +216,6 @@ public class Weather extends SettingsPreferenceFragment implements
             Settings.System.putInt(mResolver,
                     Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR, intHex);
             preference.setSummary(hex);
-            return true;
-	} else if (preference == mLockClockFonts) {
-            Settings.System.putInt(mResolver, Settings.System.LOCK_CLOCK_FONTS,
-                    Integer.valueOf((String) newValue));
-            mLockClockFonts.setValue(String.valueOf(newValue));
-            mLockClockFonts.setSummary(mLockClockFonts.getEntry());
             return true;
         }
         return false;
