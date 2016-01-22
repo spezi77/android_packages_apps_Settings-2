1ae4d70e0bbc9954d076e8d3c363a005715d0496
LorDClockaN <davor@losinj.com>
2016-01-22 22:36:00 +0100

    Squash: transparency options from AICP (2/2)
    https://github.com/AICP/packages_apps_AicpExtras/commit/155ee77957328b4c59fe60ec829b878eff54c9bd
    https://github.com/AICP/packages_apps_AicpExtras/commit/05e5f262bb64a03f49225db42dc41a13cc42f1e3
    https://github.com/AICP/packages_apps_AicpExtras/commit/2280d9d5c5f6440ff2dd7ca14530869763e4a3db
    https://github.com/AICP/packages_apps_AicpExtras/commit/d369ead520f546665081431a8eff19ec9db23af9
    https://github.com/AICP/packages_apps_AicpExtras/commit/f962a71c7b23a27b06c99d3cf0fa9c4593d6c4bc
    
    Squashed and converted to settings package
    
    Change-Id: Ib56c70fa06c926e4a18073b9bf93b4d313c49daf
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/transparency_porn_layout.xml
    	src/com/lordclockan/aicpextras/TransparencyPornFragment.java

---
+++ src/com/android/settings/cyanogenmod/Transparency.java
@@ -0,0 +1,123 @@
+package com.android.settings.cyanogenmod;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+
+import com.android.settings.widget.SeekBarPreferenceCham;
+
+public class Transparency extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+        private static final String PREF_QS_TRANSPARENT_SHADE = "qs_transparent_shade";
+        private static final String PREF_QS_TRANSPARENT_HEADER = "qs_transparent_header";
+        private static final String PREF_TRANSPARENT_VOLUME_DIALOG = "transparent_volume_dialog";
+        private static final String PREF_TRANSPARENT_POWER_MENU = "transparent_power_menu";
+        private static final String PREF_TRANSPARENT_POWER_DIALOG_DIM = "transparent_power_dialog_dim";
+
+        private SeekBarPreferenceCham mQSShadeAlpha;
+        private SeekBarPreferenceCham mQSHeaderAlpha;
+        private SeekBarPreferenceCham mVolumeDialogAlpha;
+        private SeekBarPreferenceCham mPowerMenuAlpha;
+        private SeekBarPreferenceCham mPowerDialogDim;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.transparency);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            final ContentResolver resolver = getActivity().getContentResolver();
+
+            // QS shade alpha
+            mQSShadeAlpha =
+                    (SeekBarPreferenceCham) prefSet.findPreference(PREF_QS_TRANSPARENT_SHADE);
+            int qSShadeAlpha = Settings.System.getInt(resolver,
+                    Settings.System.QS_TRANSPARENT_SHADE, 255);
+            mQSShadeAlpha.setValue(qSShadeAlpha / 1);
+            mQSShadeAlpha.setOnPreferenceChangeListener(this);
+
+            // QS header alpha
+            mQSHeaderAlpha =
+                    (SeekBarPreferenceCham) prefSet.findPreference(PREF_QS_TRANSPARENT_HEADER);
+            int qSHeaderAlpha = Settings.System.getInt(resolver,
+                    Settings.System.QS_TRANSPARENT_HEADER, 255);
+            mQSHeaderAlpha.setValue(qSHeaderAlpha / 1);
+            mQSHeaderAlpha.setOnPreferenceChangeListener(this);
+
+            // Volume dialog alpha
+            mVolumeDialogAlpha =
+                    (SeekBarPreferenceCham) prefSet.findPreference(PREF_TRANSPARENT_VOLUME_DIALOG);
+            int volumeDialogAlpha = Settings.System.getInt(resolver,
+                    Settings.System.TRANSPARENT_VOLUME_DIALOG, 255);
+            mVolumeDialogAlpha.setValue(volumeDialogAlpha / 1);
+            mVolumeDialogAlpha.setOnPreferenceChangeListener(this);
+
+            // Power menu alpha
+            mPowerMenuAlpha =
+                    (SeekBarPreferenceCham) prefSet.findPreference(PREF_TRANSPARENT_POWER_MENU);
+            int powerMenuAlpha = Settings.System.getInt(resolver,
+                    Settings.System.TRANSPARENT_POWER_MENU, 100);
+            mPowerMenuAlpha.setValue(powerMenuAlpha / 1);
+            mPowerMenuAlpha.setOnPreferenceChangeListener(this);
+
+            // Power/reboot dialog dim
+            mPowerDialogDim =
+                    (SeekBarPreferenceCham) prefSet.findPreference(PREF_TRANSPARENT_POWER_DIALOG_DIM);
+            int powerDialogDim = Settings.System.getInt(resolver,
+                    Settings.System.TRANSPARENT_POWER_DIALOG_DIM, 50);
+            mPowerDialogDim.setValue(powerDialogDim / 1);
+            mPowerDialogDim.setOnPreferenceChangeListener(this);
+        }
+
+	@Override
+    	protected int getMetricsCategory() {
+            return MetricsLogger.MAIN_SETTINGS;
+    	}
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            int alpha = (Integer) newValue;
+            if (preference == mQSShadeAlpha) {
+                Settings.System.putInt(resolver,
+                        Settings.System.QS_TRANSPARENT_SHADE, alpha * 1);
+                return true;
+            } else if (preference == mQSHeaderAlpha) {
+                Settings.System.putInt(resolver,
+                        Settings.System.QS_TRANSPARENT_HEADER, alpha * 1);
+                return true;
+            } else if (preference == mVolumeDialogAlpha) {
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSPARENT_VOLUME_DIALOG, alpha * 1);
+                return true;
+            } else if (preference == mPowerMenuAlpha) {
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSPARENT_POWER_MENU, alpha * 1);
+                return true;
+            } else if (preference == mPowerDialogDim) {
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSPARENT_POWER_DIALOG_DIM, alpha * 1);
+                return true;
+            }
+            return false;
+        }
+}
