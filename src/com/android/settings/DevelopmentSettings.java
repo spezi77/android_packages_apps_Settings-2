f742555566d83240bbdcc5a7689fdc3f81f922d6
scott <scott@ubuntu>
2016-03-11 21:43:43 -0500

    undo last fix... wasnt a fix.
    Change-Id: I0aa5b5f69ba09941c2f94b517f3831ddd81b57ff
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -305,6 +305,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private SwitchPreference mSelinux;
 
+    private SwitchPreference mColorTemperaturePreference;
+
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
 
     private final ArrayList<SwitchPreference> mResetSwitchPrefs
